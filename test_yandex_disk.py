8a03f14c7c96dab5013eb61009046fc8804b145d
Alena <alena@MacBook-Pro-Alena.local>
2022-10-10 18:28:04 +0300

    the syntax has been edited

--- test_yandex_disk.py
+++ test_yandex_disk.py
@@ -9,20 +9,21 @@ headers = {
 }
 def test_create_folder(path):
     response1 = requests.put(f'{url}?path={path}', headers=headers)
-    print(response1.status_code)
+    assert response1.status_code == 201
 
 def test_copy_file(path_from, path_to):
     response2 = requests.post(f'{url}/copy?from={path_from}&path={path_to}', headers=headers)
-    print(response2.status_code)
+    assert response2.status_code == 201
 
 def test_rename_file(path_from, path_to):
     response3 = requests.post(f'{url}/move?from={path_from}&path={path_to}', headers=headers)
-    print(response3.status_code)
+    assert response3.status_code == 201
+    assert path_from != path_to
 
 def test_log_out_user(path):
     url = "https://passport.yandex.ru/passport"
     response4 = requests.get(f'{url}?mode={path}', headers=headers)
-    print(response4.status_code)
+    assert response4.status_code == 200
 
 test_create_folder('New API Folder')
 test_copy_file("/Файл для копирования.docx","/New API Folder/Файл для копирования.docx")
